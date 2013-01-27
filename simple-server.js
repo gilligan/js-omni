9c8f18d2a9503b45816c0bcde0579279556a025b
Tobias Pflug <tobias.pflug@gmail.com>
2013-01-27 21:17:35 +0100

    structure cleanup

--- simple-server.js
+++
@@ -1,8 +0,0 @@
-var net = require('net');
-var server = net.createServer(function (socket) {
-    socket.write("Echo server\r\n");
-    socket.on('data', function (data) {
-        process.stdout.write(data);
-    });
-});
-server.listen(1338, "127.0.0.1");
